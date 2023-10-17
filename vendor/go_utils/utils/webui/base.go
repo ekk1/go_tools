63671d401ab922cba74092341d84a6c176aedaa7
Your Name <you@example.com>
2023-10-17 18:01:19 +0800

    [update] vendor

--- vendor/go_utils/utils/webui/base.go
+++ vendor/go_utils/utils/webui/base.go
@@ -10,6 +10,7 @@ var BaseHTML string = `<!DOCTYPE html>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>%s</title>
+  <link rel="icon" href="data:;base64,iVBORw0KGgo=">
   <style>
   h1 {
     color: DarkMagenta;
