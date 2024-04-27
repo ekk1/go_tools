86daf1aab49ba6138e728b4a2e588adb6eebeafd
Your Name <you@example.com>
2024-04-27 16:16:44 +0800

    update

--- cmd/g2/handlers.go
+++ cmd/g2/handlers.go
@@ -7,6 +7,9 @@ import (
 	"net/http"
 )
 
+type ReturnData struct {
+}
+
 func handleUserInput(w http.ResponseWriter, req *http.Request) {
 	gasData := req.FormValue("gas_data")
 	e := &event.PlayerEvent{}
@@ -17,4 +20,31 @@ func handleUserInput(w http.ResponseWriter, req *http.Request) {
 	GlobalEventChannel <- e
 	<-e.Finished
 	w.Write([]byte(e.Output))
+}
+
+func handleGetData(w http.ResponseWriter, req *http.Request) {
+	userID := req.FormValue("user_id")
+	cityName := req.FormValue("city_name")
+	if userID == "" {
+		utils.LogPrintError("Invalid user id")
+		w.Write([]byte("Invalid user id"))
+		return
+	}
+	if cityName == "" {
+		utils.LogPrintError("Invalid city name")
+		w.Write([]byte("Invalid city name"))
+		return
+	}
+	if _, ok := GlobalPlayerList[userID]; !ok {
+		utils.LogPrintError("User not found")
+		w.Write([]byte("User not found"))
+		return
+	}
+	p := GlobalPlayerList[userID]
+	if _, ok := p.CityList[cityName]; !ok {
+		utils.LogPrintError("City not found")
+		w.Write([]byte("City not found"))
+		return
+	}
+
 }
