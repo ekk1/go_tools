f38a1be0942c957f9c4029be026fcd5be9eb4bb7
Your Name <you@example.com>
2024-04-29 23:33:55 +0800

    [update] basic layout

--- cmd/mozz/frontend/main.js
+++ cmd/mozz/frontend/main.js
@@ -1,2 +1,12 @@
 var now = new Date().toISOString().split('T')[0];
 document.getElementById("dateinput").value = now;
+
+for (var i = 0; i < 5; i++) {
+    var _tr = document.createElement("tr");
+    for (var j = 0; j < 3; j++) {
+        var _data = document.createElement("td");
+        _data.innerHTML = "test";
+        _tr.appendChild(_data);
+    }
+    document.getElementById("record-list").appendChild(_tr);
+}
